91999ba4e78722f59881d658668e1e8da3899af5
Vivesh kumar <vivesh103@gmail.com>
2025-01-12 22:31:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-//complete this code
+// Define the Animal class
 class Animal {
-	constructor(species){
-		this.species = species;
-	}
-	get species(){
-		return this.species;
-	}
-	makeSound(){
-		console.log(`The ${this.species} makes a sound`);
-	}
+  constructor(species) {
+    this._species = species;
+  }
+
+  // Getter for species
+  get species() {
+    return this._species;
+  }
+
+  // Method to make a sound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
 }
 
+// Define the Cat class extending Animal
+class Cat extends Animal {
+  // Method to purr
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Define the Dog class extending Animal
 class Dog extends Animal {
-	bark(){
-		console.log("woof");
-}
-}
-
-class Cat extends Animal {}
-	
-	purr(){
-		console.log("purr");
-	}
+  // Method to bark
+  bark() {
+    console.log("woof");
+  }
 }
 
-// Do not change the code below this line
+// Expose the classes to the global `window` object for Cypress to access
 window.Animal = Animal;
-window.Dog = Dog;
 window.Cat = Cat;
+window.Dog = Dog;
